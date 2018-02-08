efe451fa9e078db553b2b0929aca20d21a9fd8f5
Varun Amachi <varunamachi@gmail.com>
2018-02-08 21:52:22 +0530

    [WIP] model

--- entity/model.go
+++ entity/model.go
@@ -1,5 +1,20 @@
 package entity
 
+import (
+	"github.com/varunamachi/vaali/vparam"
+)
+
+type VariableAccess int
+
 //Entity - represents a entiry with variables
 type Entity struct {
+	Name     string `json:"name" bson:"name"`
+	Type     string `json:"type" bson:"type"`
+	Location string `json:"location" bson:"location"`
+	Owner    string `json:"owner" bson:"owner"`
+}
+
+//Variable - reprents a read-only or read-write variable
+type Variable struct {
+	Param vparam.Param
 }
