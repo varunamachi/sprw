c726a5cd94a268ac2e4daefdcdee3c4b454d34ca
Varun Amachi <varunamachi@gmail.com>
2018-02-20 20:33:56 +0530

    Entity handlers

--- entity/model.go
+++ entity/model.go
@@ -3,21 +3,27 @@ package entity
 import (
 	"time"
 
+	"gopkg.in/mgo.v2/bson"
+
 	"github.com/varunamachi/vaali/vcmn"
 )
+
+//EntityColn - entity collection name
+const EntityColn = "entity"
 
 //VariableAccess - access level of variable
 type VariableAccess int
 
 //Entity - represents a entiry with variables
 type Entity struct {
-	Name       string       `json:"name" bson:"name"`
-	Type       string       `json:"type" bson:"type"`
-	Location   string       `json:"location" bson:"location"`
-	Owner      string       `json:"owner" bson:"owner"`
-	Variables  []vcmn.Param `json:"variables" bson:"variables"`
-	Readers    []string     `json:"readers" bson:"readers"`
-	Writers    []string     `json:"writers" bson:"writers"`
-	CreateAt   time.Time    `json:"createdAt" bson:"createdAt"`
-	ModifiedAt time.Time    `json:"modifiedAt" bson:"modifiedAt"`
+	ID         bson.ObjectId `json:"_id" bson:"_id"`
+	Name       string        `json:"name" bson:"name"`
+	Type       string        `json:"type" bson:"type"`
+	Location   string        `json:"location" bson:"location"`
+	Owner      string        `json:"owner" bson:"owner"`
+	Variables  []vcmn.Param  `json:"variables" bson:"variables"`
+	Readers    []string      `json:"readers" bson:"readers"`
+	Writers    []string      `json:"writers" bson:"writers"`
+	CreateAt   time.Time     `json:"createdAt" bson:"createdAt"`
+	ModifiedAt time.Time     `json:"modifiedAt" bson:"modifiedAt"`
 }
