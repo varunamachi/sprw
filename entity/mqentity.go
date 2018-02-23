f98b4e33f4ce45f6cc925ea69a79f62720ff4bf6
Varun Amachi <varunamachi@gmail.com>
2018-02-23 21:47:05 +0530

    Sprw module impl

--- entity/mqentity.go
+++ entity/mqentity.go
@@ -1,1 +1,20 @@
 package entity
+
+import (
+	"github.com/varunamachi/vaali/vapp"
+)
+
+//Init - sparrow-entity initialization
+func Init(app *vapp.App) (err error) {
+	return err
+}
+
+//Setup - setup function for entity module
+func Setup(app *vapp.App) (err error) {
+	return err
+}
+
+//Reset - sparrow-entity reset
+func Reset(app *vapp.App) (err error) {
+	return err
+}
---
+++ module.go
@@ -0,0 +1,20 @@
+package sprw
+
+import (
+	"github.com/varunamachi/sprw/entity"
+	"github.com/varunamachi/vaali/vapp"
+	"gopkg.in/urfave/cli.v1"
+)
+
+//NewModule - creates new sparrow module
+func NewModule() *vapp.Module {
+	return &vapp.Module{
+		Name:        "sprw",
+		Description: "The sparrow server",
+		Endpoints:   entity.GetEndpoints(),
+		Commands:    []cli.Command{},
+		Initialize:  entity.Init,
+		Setup:       entity.Setup,
+		Reset:       entity.Reset,
+	}
+}
